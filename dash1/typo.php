0c1c948a72cb9e1fdbe67746000c91c10741b3d6
ADjan2023 <banyin.djan@ashesi.edu.gh>
2022-05-08 02:32:21 +0000

    course description

--- dash1/typo.php
+++ dash1/typo.php
@@ -578,103 +578,40 @@ if(isset($_SESSION['email'])) {
 
        //While loop to print out data in a table form
   ?></h1>
-                      <h2 class="pb-2 display-5">Sections & Modal Names H2</h2>
-                      <h3 class="pb-2 display-5">Articles & Block Headings H3</h3>
-                      <h4 class="pb-2 display-5">Random Tiny Heading H4</h4>
-                      <h5 class="pb-2 display-5">Random Tiny Heading H5</h5>
-                      <h6 class="pb-4 display-5">Random Tiny Heading H6</h6>
+                      
                     </div>
                     <div class="typo-articles">
+                        <br>
                       <p>
-                        The unique stripes of zebras make them one of the animals most familiar to people. They occur in a variety of habitats, such
-                        as grasslands, savannas,
-                        <span class="bg-flat-color-1 text-light">woodlands</span>, thorny scrublands,
-                        <span class="clickable-text">mountains</span>
-                        , and coastal hills. However, various anthropogenic factors have had a severe impact on zebra populations, in particular
-                        hunting for skins and habitat destruction. Grévy's zebra and the mountain
-                        <mark>highlighted text</mark> zebra are endangered.</p>
-                      <blockquote class="blockquote mt-3 text-right">
-                        <p>
-                          Blockquotes. However, various anthropogenic factors have had a severe impact on zebra populations, in particular hunting
-                          for skins. </p>
-                        <footer class="blockquote-footer">Jefferey Lebowski</footer>
-                      </blockquote>
-                      <p>
-                        lthough zebra species may have overlapping ranges, they do not interbreed. In captivity, plains zebras have been crossed
-                        with mountain zebras. The hybrid foals
-                        <span class="bg-flat-color-1 text-light">selected text</span> lacked a dewlap and resembled their
-                      </p>
-                    </div>
-                    <div class="vue-lists">
-                      <h2 class="mb-4">Lists</h2>
-                      <div class="row">
-                        <div class="col-md-6">
-                          <h3>Unordered</h3>
-                          <ul>
-                            <li>
-                              A wide variety of hypotheses have been proposed to account for the evolution of the striking stripes of zebras.
-                            </li>
-                            <li>The more traditional of these (1 and 2, below) relate to camouflage.</li>
-                            <li>The vertical striping may help the zebra hide in the grass by disrupting its outline.</li>
-                            <li>
-                              In addition, even at moderate distances, the striking striping merges to an apparent grey.
-                              <ul class="vue-list-inner">
-                                <li>However, the camouflage has been contested with arguments that most of a zebra's predator.</li>
-                                <li>Such as lions and hyenas cannot see well at a distance.</li>
-                                <li>More likely to have smelled or heard a zebra.</li>
-                              </ul>
-                            </li>
-                            <li>Before seeing it from a distance, especially at night.</li>
-                          </ul>
-                        </div>
-                        <div class="col-md-6 text-left">
-                          <div>
-                            <h3>Ordered</h3>
-                            <ol class="vue-ordered">
-                              <li>
-                                A wide variety of hypotheses have been proposed to account for the evolution of the striking stripes of zebras.
-                              </li>
-                              <li>The more traditional of these (1 and 2, below) relate to camouflage.</li>
-                              <li>The vertical striping may help the zebra hide in the grass by disrupting its outline.</li>
-                              <li>
-                                In addition, even at moderate distances, the striking striping merges to an apparent grey.
-                                <ul class="vue-list-inner">
-                                  <li>However, the camouflage has been contested with arguments that most of a zebra's predator.
-                                  </li>
-                                  <li>Such as lions and hyenas cannot see well at a distance.</li>
-                                  <li>More likely to have smelled or heard a zebra.</li>
-                                </ul>
-                              </li>
-                              <li>Before seeing it from a distance, especially at night.</li>
-                            </ol>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="vue-misc">
-                      <h2 class="display-5 my-3">Misc</h2>
-                      <div class="row">
-                        <div class="col-md-6">
-                          <h3>Address</h3>
-                          <address class="mt-3">
-                            <strong>SJØNNA</strong>
-                            <br> Nezalezhnasti Ave, 13 - 28A
-                            <br> Minsk, Belarus, 220141
-                            <br> +375 29 319-53-98
-                            <br>
-                            <br>
-                            <b>Vasili Savitski</b>
-                            <br>
-                            <a href="mailto">hello@examplemail.com</a>
+                         <?php
+              
+    ob_start();
+       include "../dbconnect.php"; // Using database connection file here
+        ob_end_clean();
+     
+      if(isset($_POST['info'])){
+        $cid=$_POST['cid'];
+            
+          
+        $records1 = mysqli_query($db,"SELECT * FROM electives where (`Course_Id`= '$cid') "); // fetch data from database
+        if($records1){
+           
+        while($data = mysqli_fetch_array($records1)){
+            
+            
+         echo $data['Course Description']; 
+       
+        }}
+         
 
-                          </address>
-                        </div>
-                        <div class="col-md-6">
-                          <h3 class="mb-3">Well</h3>
-                          <div class="jumbotron">
-                            Zebras have excellent eyesight. It is believed that they can see in color. Like most ungulates, the zebra's eyes are on the
-                            sides of its head, giving it a wide field of view.
-                          </div>
+
+}
+
+
+       //While loop to print out data in a table form
+  ?>
+                     </p>
+                        
                         </div>
                       </div>
                     </div>
