cd1913538b8970f1a90d914009254287ac882e99
ADjan2023 <banyin.djan@ashesi.edu.gh>
2022-05-08 01:55:51 +0000

    chatbot integration

--- dash1/home.php
+++ dash1/home.php
@@ -1024,7 +1024,17 @@ if(isset($_SESSION['email'])) {
 
     <!-- Main JS-->
     <script src="js/main.js"></script>
-
+<!-- Start of ChatBot (www.chatbot.com) code -->
+<script type="text/javascript">
+    window.__be = window.__be || {};
+    window.__be.id = "6277132172f09700079f066e";
+    (function() {
+        var be = document.createElement('script'); be.type = 'text/javascript'; be.async = true;
+        be.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.chatbot.com/widget/plugin.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(be, s);
+    })();
+</script>
+<!-- End of ChatBot code -->
 </body>
 
 </html>
